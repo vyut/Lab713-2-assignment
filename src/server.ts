dfe2b0c8f3ed9c40e39a2160150ab2f07a0793e8
Weerayuth Uarjaipra <yut@Yuts-MacBook-Air-M2.local>
2025-02-13 19:59:12 +0700

    Task 10 - No. 1-2

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response } from 'express';
-import { parse } from 'path';
 
 const app = express();
 const port = 3000;
@@ -11,6 +10,122 @@ app.get('/', (req: Request, res: Response) => {
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
 });
+
+// app.get('/test', (req, res) => {
+//     let returnObj = {
+//         name: 'test',
+//         age: 20,
+//         address: 'Thai'
+//     }
+//     res.send(returnObj);
+// });
+
+app.get('/test', (req: Request, res: Response) => {
+    const id = req.query.id;
+    const output = `id: ${id}`;
+    res.send(output)
+});
+
+interface Event {
+    id: number;
+    category: string;
+    title: string;
+    description: string;
+    location: string;
+    date: string;
+    time: string;
+    petsAllowed: boolean;
+    organizer: string;
+};
+
+const events: Event[] = [
+    {
+        id: 1,
+        category: "Music",
+        title: "Concert",
+        description: "A live concert",
+        location: "London",
+        date: "2021-07-01",
+        time: "19:00",
+        petsAllowed: false,
+        organizer: "Live Nation",
+    },
+    {
+        id: 2,
+        category: "Sport",
+        title: "Football match",
+        description: "A football match",
+        location: "Manchester",
+        date: "2021-07-02",
+        time: "20:00",
+        petsAllowed: false,
+        organizer: "FIFA",
+    },
+    {
+        id: 3,
+        category: "Music",
+        title: "Festival",
+        description: "A music festival",
+        location: "Bristol",
+        date: "2021-07-03",
+        time: "21:00",
+        petsAllowed: true,
+        organizer: "Glastonbury",
+    },
+    {
+        id: 4,
+        category: "Sport",
+        title: "Tennis match",
+        description: "A tennis match",
+        location: "Liverpool",
+        date: "2021-07-04",
+        time: "22:00",
+        petsAllowed: false,
+        organizer: "Wimbledon",
+    },
+    {
+        id: 5,
+        category: "Music",
+        title: "Concert",
+        description: "A live concert",
+        location: "London",
+        date: "2021-07-01",
+        time: "19:00",
+        petsAllowed: false,
+        organizer: "Live Nation",
+    },
+    {
+        id: 6,
+        category: "Sport",
+        title: "Football match",
+        description: "A football match",
+        location: "Manchester",
+        date: "2021-07-02",
+        time: "20:00",
+        petsAllowed: false,
+        organizer: "FIFA",
+    },
+]
+
+app.get("/events", (req, res) => {
+    if (req.query.category) {
+        const category = req.query.category as string;
+        const filteredEvents = events.filter((event) => event.category === category);
+        res.send(filteredEvents);
+    } else {
+        res.json(events);
+    }
+});
+
+app.get("/events/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const event = events.find((event) => event.id === id);
+    if (event) {
+        res.json(event);
+    } else {
+        res.status(404).send("Event not found");
+    }
+})
 
 interface Book {
     id: number;
