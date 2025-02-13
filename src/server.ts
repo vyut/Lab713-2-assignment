dbd74f96802994a3de1b863389cf2ba8675db4b0
Weerayuth Uarjaipra <yut@Yuts-MacBook-Air-M2.local>
2025-02-13 19:00:42 +0700

    Task 8 - No. 9

--- src/server.ts
+++ src/server.ts
@@ -11,11 +11,17 @@ app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
 });
 
-app.get('/test', (req, res) => {
-    let returnObj = {
-        name: 'test',
-        age: 20,
-        address: 'Thai'
-    }
-    res.send(returnObj);
+// app.get('/test', (req, res) => {
+//     let returnObj = {
+//         name: 'test',
+//         age: 20,
+//         address: 'Thai'
+//     }
+//     res.send(returnObj);
+// });
+
+app.get('/test', (req: Request, res: Response) => {
+    const id = req.query.id;
+    const output = `id: ${id}`;
+    res.send(output)
 });
